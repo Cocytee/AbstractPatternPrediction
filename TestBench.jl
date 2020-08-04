488f5aa95c26689d8e50ec12cce60944cc551dba
Cocytie <Cocytie@protonmail.com>
2020-08-04 18:04:04 +0200

    Testing Preparation

--- TestBench.jl
+++ TestBench.jl
@@ -1,1 +1,9 @@
 #Try that shit !  
+
+using APPcore
+
+(f,ext) = fileMUX("TestDataSet.csv")
+
+#DS1 = ExctractorBuilder("TestDataSet.csv",3,2;cSets=1,cCat=2,dataStartAt=(4,3),dataStopAt=(10,42))
+#DS2 = ExctractorBuilder("TestDataSet.csv",3,2;cSets=1,cCat=2,dataStartAt=(4,3),dataStopAt=(10,42))
+#DS3 = ExctractorBuilder("DSConfig.txt")
