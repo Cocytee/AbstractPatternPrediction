02139af5ab96ed18a66d8ca788e56b9e064961ce
Oz <jules.skubich@non.se.com>
2020-08-05 11:52:19 +0200

    Testing Wrong and Good filename

--- TestBench.jl
+++ TestBench.jl
@@ -4,8 +4,10 @@ include("./APPcore.jl")
 using .APPcore
 
 println(" ~ Testing FileMUX")
-(f,ext) = APPcore.FileMUX("TestDataSet.csv")
-println("\t -> ",f," :: ",ext)
+r = APPcore.FileMUX("TestDataSet.csv")
+println("\t -> ",r)
+r = APPcore.FileMUX("NoOne.csv")
+println("\t -> ",r)
 
 #DS1 = ExctractorBuilder("TestDataSet.csv",3,2;cSets=1,cCat=2,dataStartAt=(4,3),dataStopAt=(10,42))
 #DS2 = ExctractorBuilder("TestDataSet.csv",3,2;cSets=1,cCat=2,dataStartAt=(4,3),dataStopAt=(10,42))
