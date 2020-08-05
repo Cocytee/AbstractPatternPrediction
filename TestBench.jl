e66b58952298ab06b79dc02a7564f2c0f0c97277
Oz <jules.skubich@non.se.com>
2020-08-05 14:12:57 +0200

    Minor Changes

--- TestBench.jl
+++ TestBench.jl
@@ -5,8 +5,11 @@ using .APPcore
 
 DSS = ExctractorBuilder("None.csv",1,1)
 DSSbis = ExctractorBuilder("None.cv",1,1)
+
 DSS2 = ExctractorBuilder("TestDataSet.csv",1,1)
 DSS2bis = ExctractorBuilder("TestDataSet.csv",3,2)
+
 DSS3 = ExctractorBuilder("TestDataSet.csv",1,1;dataStartAt=(4,3))
 DSS3bis = ExctractorBuilder("TestDataSet.csv",1,1;dataStartAt=(4,3),dataStopAt=(52,41))
+
 #DSS4 = ExctractorBuilder("DSConfig.txt")
