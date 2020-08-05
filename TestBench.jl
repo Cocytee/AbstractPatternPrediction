debc4901606fd6742778b17d4cc79a928c1facc5
Oz <jules.skubich@non.se.com>
2020-08-05 14:41:08 +0200

    ?

--- TestBench.jl
+++ TestBench.jl
@@ -3,6 +3,7 @@
 include("./APPcore.jl")
 using .APPcore
 
+<<<<<<< Updated upstream
 DSS = ExctractorBuilder("None.csv",1,1)
 DSSbis = ExctractorBuilder("None.cv",1,1)
 
@@ -13,3 +14,12 @@ DSS3 = ExctractorBuilder("TestDataSet.csv",1,1;dataStartAt=(4,3))
 DSS3bis = ExctractorBuilder("TestDataSet.csv",1,1;dataStartAt=(4,3),dataStopAt=(52,41))
 
 #DSS4 = ExctractorBuilder("DSConfig.txt")
+=======
+DSS = APPcore.ExctractorBuilder("None.csv",1,1)
+DSSbis = APPcore.ExctractorBuilder("None.cv",1,1)
+DSS2 = APPcore.ExctractorBuilder("TestDataSet.csv",1,1)
+DSS2bis = APPcore.ExctractorBuilder("TestDataSet.csv",3,2)
+DSS3 = APPcore.ExctractorBuilder("TestDataSet.csv",1,1;dataStartAt=(4,3))
+DSS3bis = APPcore.ExctractorBuilder("TestDataSet.csv",1,1;dataStartAt=(4,3),dataStopAt=(52,41))
+#DSS4 = APPcore.ExctractorBuilder("DSConfig.txt")
+>>>>>>> Stashed changes
